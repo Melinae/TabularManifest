5ec2172eb57f2afc1eab652afd8be07fb05f3675
Will Beasley <wibeasley@hotmail.com>
2014-03-20 15:02:48 -0500

    Changing naming scheme of files

--- playgrounds/README.md
+++
@@ -1,1 +0,0 @@
-This folder is for snippets of code that we're exploring.  No production code should exist in this directory.
--- playgrounds/qualifying_gam.R
+++
@@ -1,8 +0,0 @@
-rm(list=ls(all=TRUE))
-require(ggplot2)
-
-# beaver1
-ggplot(beaver1, aes(x=time, y=activ)) +
-  geom_point() +
-  geom_smooth(method=mgcv::gam)
-# geom_smooth(method="mgcv::gam")
